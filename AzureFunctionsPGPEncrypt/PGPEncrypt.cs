e19e19dbd52fc8f0fa85ecff03c444e4ef029ea9
nwicke-pk <nwicke@prokarma.com>
2020-02-12 21:49:43 -0800

    wrote file

--- AzureFunctionsPGPEncrypt/PGPEncrypt.cs
+++ AzureFunctionsPGPEncrypt/PGPEncrypt.cs
@@ -14,6 +14,11 @@ using System.Text;
 using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Azure.Identity;
+using Azure.Storage.Blobs;
 
 namespace AzureFunctionsPGPEncrypt
 {
@@ -25,7 +30,8 @@ namespace AzureFunctionsPGPEncrypt
         [FunctionName(nameof(PGPEncrypt))]
         public static async Task<IActionResult> RunAsync(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
-        HttpRequest req, ILogger log)
+        HttpRequest req,
+            ILogger log)
         {
             log.LogInformation($"C# HTTP trigger function {nameof(PGPEncrypt)} processed a request.");
 
@@ -61,6 +67,19 @@ namespace AzureFunctionsPGPEncrypt
             byte[] data = Convert.FromBase64String(publicKeyBase64);
             string publicKey = Encoding.UTF8.GetString(data);
             req.EnableRewind(); //Make RequestBody Stream seekable
+
+            /*  CloudStorageAccount account = new CloudStorageAccount(;
+              CloudBlobClient blobClient = account.CreateCloudBlobClient();
+              CloudBlobContainer container = blobClient.GetContainerReference("names-in");
+
+              var blobReference = container.GetBlockBlobReference(blobPath);
+
+              string originalName = blobReference.DownloadText();
+
+              return originalName;*/
+
+            await CreateBlockBlobAsync("pktestdatastorage", "testdata", "new_file_now.csv");
+
             Stream encryptedData = await EncryptAsync(req.Body, publicKey);
 
             return new OkObjectResult(encryptedData);
@@ -105,5 +124,41 @@ namespace AzureFunctionsPGPEncrypt
             stream.Position = 0;
             return stream;
         }
+
+
+        private async static Task CreateBlockBlobAsync(string accountName, string containerName, string blobName)
+        {
+
+            // Construct the blob container endpoint from the arguments.
+            string containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}",
+                                                        accountName,
+                                                        containerName);
+
+   
+            // Get a credential and create a client object for the blob container.
+            BlobContainerClient containerClient = new BlobContainerClient("DefaultEndpointsProtocol=https;AccountName=pktestdatastorage;AccountKey=AakIuSa76YTTNjs6Xm2TNuH7VLsBg+Y5KYJHB034n0KDpHT4jFrmibKrNtYo5y1rwCG/5r+FC66rxeX6DpoLgg==;EndpointSuffix=core.windows.net","testdata");
+
+            try
+            {
+                // Create the container if it does not exist.
+                await containerClient.CreateIfNotExistsAsync();
+
+                // Upload text to a new block blob.
+                string blobContents = "This is a block blob.";
+                byte[] byteArray = Encoding.ASCII.GetBytes(blobContents);
+
+                using (MemoryStream stream = new MemoryStream(byteArray))
+                {
+                    await containerClient.UploadBlobAsync(blobName, stream);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+
     }
 }
